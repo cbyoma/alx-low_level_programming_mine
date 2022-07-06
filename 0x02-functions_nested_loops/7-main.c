5472276f6e15bebc86954cdc46140921c716b5f6
cbyoma <cbyoma@gmail.com>
2022-07-06 00:00:09 -0700

    Last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -11,7 +11,9 @@ int main(void)
 {
 	int r;
 
-	r = print_last_digit(INT_MIN);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
 	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
