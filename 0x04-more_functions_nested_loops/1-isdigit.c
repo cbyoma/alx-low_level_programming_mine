5cd1db69fbcb3b662a9cc556efd2a3c4873bed18
cbyoma <cbyoma@gmail.com>
2022-07-07 04:51:01 -0700

    Is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,13 +9,8 @@
  */
 int _isdigit(int c)
 {
-	for (c = 0; c < 10; c++)
-	{
-		if (c == 1)
-			c = 1;
-		else
-			c = 0;
-	}
-
-	return (c);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
