10f1ac683b04700b2eef6595500f3d3b81f6b9db
cbyoma <cbyoma@gmail.com>
2022-07-12 09:00:31 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - This loop will be caaled by the main loop
+ * @n: This ia a pointer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
